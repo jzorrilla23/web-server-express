6d15337901921249376b457270be0ec26b1de6e6
jzorrilla23 <jzorrilla@intellisys.com.do>
2022-02-17 18:16:59 -0400

    Added a static web and configue express to handle the links.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.use(express.static('public'))
 app.get('/',  (req, res) =>{
   res.send('Hello World')
 })
@@ -9,5 +10,22 @@ app.get('/',  (req, res) =>{
 app.get('/josue/:age', (req, res) => {
     res.send(`<p> Josue Zorrilla ${req.params.age} </p>`)
 })
+
+app.get('/index', (req,res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/generic', (req, res)=>{
+    res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res)=>{
+    res.sendFile(__dirname + '/public/elements.html')
+})
+app.get('*', (req, res)=>{
+    res.sendFile(__dirname + '/public/404.html')
+})
+
+
 
 app.listen(port, ()=>{ console.log(`Listen on port ${port}`)})
